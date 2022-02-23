56b34d2e1b7d1d764e44e4fd21c1dc5eabb86c8f
Bitna <digital_bnalee-at-640252953590>
2022-02-23 11:29:50 +0900

    upload build

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
       <Router>
     <Routes>
-      <Route path="/movie/:id" element={<Detail />}></Route>
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Detail />}></Route>
       <Route path="/" element={<Home />}></Route>
     </Routes>
   </Router>
